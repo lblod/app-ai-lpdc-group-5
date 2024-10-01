1719088112b5fee54e97c64a5a35db8e155b00b7
Achraf <achrafatauil@hotmail.com>
2024-10-01 13:58:00 +0200

    reset repo

--- config/reports/index.js
+++
@@ -1,9 +0,0 @@
-import lpdcBestuurseenheidReport from './lpdcBestuurseenheidReport';
-import lpdcConceptsReport from './lpdcConceptsReport';
-import instancesStuckinPublishingForLPDCReport from './instancesStuckinPublishingForLPDCReport';
-
-export default [
-  lpdcBestuurseenheidReport,
-  lpdcConceptsReport,
-  instancesStuckinPublishingForLPDCReport,
-];
--- tests/test-e2e/shared/shared.ts
+++
@@ -1,5 +0,0 @@
-export function wait(milliseconds: number): Promise<any> {
-    return new Promise(resolve => {
-        setTimeout(resolve, milliseconds);
-    });
-}
